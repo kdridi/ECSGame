7c489d3a7b5c7e8a07cbb92632be1e4a61765ebe
Karim DRIDI <karim.dridi@epitech.eu>
2015-03-02 02:19:11 +0100

    install dependency https://github.com/LaurentGomila/SFML.git

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -11,6 +11,9 @@ mkdir prefix
 cd prefix
 PREFIX_DIR=$(pwd)
 
+################################################################################
+# EntityX
+################################################################################
 cd ${ROOT_DIR}
 rm -rf work
 mkdir work
@@ -21,10 +24,33 @@ cmake ${ROOT_DIR}/submodules/github.com/alecthomas/entityx.git \
 	-DCMAKE_BUILD_TYPE=Debug \
 	-DCMAKE_INSTALL_PREFIX=${PREFIX_DIR} \
 	-DCMAKE_OSX_ARCHITECTURES=x86_64\;i386 \
-	-DENTITYX_BUILD_SHARED=ON \
-	-DENTITYX_BUILD_TESTING=ON \
+	-DENTITYX_BUILD_SHARED=OFF \
+	-DENTITYX_BUILD_TESTING=OFF \
 	-DENTITYX_DT_TYPE=double \
 	-DENTITYX_MAX_COMPONENTS=64 \
-	-DENTITYX_RUN_BENCHMARKS=ON
-make
+	-DENTITYX_RUN_BENCHMARKS=OFF
+make -j12
+make install
+
+################################################################################
+# SFML
+################################################################################
+cd ${ROOT_DIR}
+rm -rf work
+mkdir work
+cd work
+WORK_DIR=$(pwd)
+
+cmake ${ROOT_DIR}/submodules/github.com/LaurentGomila/SFML.git \
+	-DBUILD_SHARED_LIBS=OFF \
+	-DCMAKE_BUILD_TYPE=Debug \
+	-DCMAKE_INSTALL_FRAMEWORK_PREFIX=${PREFIX_DIR}/Frameworks \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX_DIR} \
+	-DCMAKE_OSX_ARCHITECTURES=x86_64\;i386 \
+	-DSFML_BUILD_DOC=OFF \
+	-DSFML_BUILD_EXAMPLES=OFF \
+	-DSFML_BUILD_FRAMEWORKS=OFF \
+	-DSFML_INSTALL_XCODE_TEMPLATES=OFF \
+	-DSFML_OPENGL_ES=OFF
+make -j12
 make install
