4d3071f837f6af08e84a6341d45f311743a5c211
Karim DRIDI <karim.dridi@epitech.eu>
2015-03-02 03:17:56 +0100

    ECS sample

---
+++ src/ecs.h
@@ -0,0 +1,22 @@
+//
+//  ecs.h
+//  arykow
+//
+//  Created by Karim DRIDI on 02/03/2015.
+//
+//
+
+#ifndef __arykow__ecs__
+#define __arykow__ecs__
+
+#include "config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    int ecs_main();
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* defined(__arykow__ecs__) */
--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,12 @@
 
 #include "arkanoid.h"
 #include "tilemap.h"
+#include "ecs.h"
 
 int main()
 {
     std::cout << "Version " << VERSION_MAJOR << "." << VERSION_MINOR << std::endl;
     std::cout << "DATA_PATH [" << DATA_PATH << "]" << std::endl;
     
-    return tilemap_main();
+    return ecs_main();
 }
